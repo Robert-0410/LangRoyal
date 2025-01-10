eb3182ef4f1885116cf4ec705619b3915afb52f1
Robert-0410 <robertbeltran@pm.me>
2025-01-09 20:25:47 -0800

    python mode

--- python/process.py
+++ python/process.py
@@ -2,6 +2,8 @@
 from typing import List
 from math import sqrt
 
+tab = "\t"
+end = "\n"
 
 input = open(file="fastapi/output.log", mode = "r")
 
@@ -40,12 +42,33 @@ def sd(data: List[float]) -> float:
             count = count + 1
     return sqrt(sum / (count -1))
 
-# TODO: Mode
+def mode(data: List[float]) -> List[float]:
+    counts = {}
+    for v in data:
+        if v in counts:
+            counts[v] = counts[v] + 1
+        else:
+            counts[v] = 1
+    biggest_count = 0
+    for c in counts:
+        if c > biggest_count:
+            biggest_count = c
+    output = list()
+    for v in counts:
+        if v == biggest_count:
+            output.append(v)
+    return output
+
 # TODO: Median
 # TODO: Max
 # TODO: Min
 
-output.write(f"Mean:\t{mean(data)}\n")
-output.write(f"SD:\t{sd(data)}\n")
+output.write(f"Mean:{tab}{mean(data)}{end}")
+output.write(f"SD:{tab}{sd(data)}{end}")
+
+modes = mode(data)
+for m in modes:
+    output.write(f"Mode:{tab}{m}{end}")
 
 output.close()
+
