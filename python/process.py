50385666ced2c3cf76eac8bd9ec094c88a59abab
Robert-0410 <robertbeltran@pm.me>
2025-01-09 19:50:48 -0800

    calculating the mean with python

--- python/process.py
+++ python/process.py
@@ -1,4 +1,7 @@
 
+from typing import List
+
+
 input = open(file="fastapi/output.log", mode = "r")
 
 timestamp = input.readline()
@@ -6,22 +9,30 @@ timestamp = input.readline()
 output = open("output.log", "a")
 output.write(timestamp)
 
-# TODO: Harvest data write size to output
+# Harvest data, write size to output
 data = []
 for line in input:
     try:
         data.append(float(line))
     except ValueError:
         print("Error line was not a float")
-
 input.close()
 output.write(f"Data Size: {len(data)}\n")
 
-# TODO: Mean
+def mean(data: List[float]) -> float:
+    sum = 0
+    count = 0
+    for v in data:
+        if type(v) == float:
+            sum = sum + v
+            count = count + 1
+    return (sum / count)
 # TODO: SD
 # TODO: Mode
 # TODO: Median
 # TODO: Max
 # TODO: Min
+
+output.write(f"Mean: {mean(data)}\n")
 
 output.close()
