b74b20c5da5535fe8a5caf51043b0ae9877c3893
robert <robertbeltran@pm.me>
2025-02-11 14:54:58 -0800

    got the program to compile and run

--- c/main.c
+++ c/main.c
@@ -7,18 +7,27 @@
 #include "server.h"
 
 void launch(struct Server *server) {
-        printf("===== WAITING FOR CONNECTION =====\n");
         char buffer[1024];
-        int address_size = sizeof(server->address);
-        int new_socket = accept(server->socket, (struct sockaddr *) &server->address, (socklen_t *) address_size);
-
-        read(new_socket, buffer, 1024);
-        printf("%s\n", buffer);
-
-        /* Responce */
         char *test_res = "Hello with C HTTP!";
-        write(new_socket, test_res, strlen(test_res));
-        close(new_socket);
+        /*int address_size = sizeof(server->address);*/
+        uint64_t address_size = sizeof(server->address);
+        int new_socket;
+        while(1) {
+                memset(buffer, 0, 1024);
+                printf("===== WAITING FOR CONNECTION =====\n");
+                new_socket = accept(
+                        server->socket,
+                        (struct sockaddr *) &server->address,
+                        (socklen_t *) address_size
+                );
+
+                read(new_socket, buffer, 1024);
+                printf("%s\n", buffer);
+
+                /* Responce */
+                write(new_socket, test_res, strlen(test_res));
+                close(new_socket);
+        }
 
 }
 
@@ -28,7 +37,7 @@ int main() {
                 SOCK_STREAM,
                 0,
                 INADDR_ANY,
-                80,
+                8000,
                 10,
                 launch
         );
